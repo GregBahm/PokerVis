f3de040f0331c7239b17af9d5d07db4522c7b947
derp derp <workingbahm@yahoo.com.sg>
2019-06-23 09:18:25 -0700

    Realized my logic is insufffficent for the 7 cards of Texas hold'em. Rethinking how I'm going to do this.

--- PokerVisUnity/Assets/CardSelector.cs
+++ PokerVisUnity/Assets/CardSelector.cs
@@ -1,104 +1,190 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CardSelector : MonoBehaviour
 {
-    [Range(0, 1)]
-    public float Progression;
-    private CardSelectorOption CurrentCard;
+    private HandState state;
+    public Transform SlotAPos;
+    public Transform SlotBPos;
+    public Transform SlotCPos;
+    public Transform SlotDPos;
+    public Transform SlotEPos;
+    public Transform SlotFPos;
+    public Transform SlotGPos;
 
-    public bool ShowOptions;
+    private CardInteractor SelectedCard;
+    private IEnumerable<CardSlot> slots;
 
-    private Transform optionsMaster;
-    private IReadOnlyList<CardSelectorOption> options;
+    public Transform CardOptionsPool;
+    private IReadOnlyList<CardInteractor> cards;
 
     private bool wasSelecting;
+    private Vector3 cursorStartDragPoint;
+    private Vector3 objStartDragPoint;
 
     private void Start()
     {
-        optionsMaster = new GameObject("Options").transform;
-        optionsMaster.parent = transform.parent;
-        options = CreateOptions();
-        CurrentCard = options[0];
+        cards = CreateCards();
+        slots = CreateSlots().ToArray();
     }
 
-    private IReadOnlyList<CardSelectorOption> CreateOptions()
+    private IEnumerable<CardSlot> CreateSlots()
     {
-        List<CardSelectorOption> ret = new List<CardSelectorOption>();
+        yield return new CardSlot(SlotAPos);
+        yield return new CardSlot(SlotBPos);
+        yield return new CardSlot(SlotCPos);
+        yield return new CardSlot(SlotDPos);
+        yield return new CardSlot(SlotEPos);
+        yield return new CardSlot(SlotFPos);
+        yield return new CardSlot(SlotGPos);
+    }
+
+    private void Update()
+    {
+        Vector3 cursorPos = GetMousePos();
+        bool isSelecting = Input.GetMouseButton(0);
+        if (isSelecting)
+        {
+            if (wasSelecting)
+            {
+                UpdateDrag(cursorPos);
+            }
+            else
+            {
+                SelectedCard = GetHoveredCard(cursorPos);
+                if (SelectedCard != null)
+                {
+                    cursorStartDragPoint = cursorPos;
+                    objStartDragPoint = SelectedCard.CardObject.transform.position;
+                }
+            }
+        }
+        else
+        {
+            if(wasSelecting && SelectedCard != null)
+            {
+                DoRelease(cursorPos);
+            }
+            SettleCardPositions();
+        }
+        wasSelecting = isSelecting;
+    }
+
+    private void SettleCardPositions()
+    {
+        HashSet<CardInteractor> cardHash = new HashSet<CardInteractor>(cards);
+        foreach (CardSlot slot in slots.Where(item => item.Occupant != null))
+        {
+            slot.Occupant.ReturnToPosition(slot);
+            cardHash.Remove(slot.Occupant);
+        }
+        foreach (CardInteractor card in cardHash)
+        {
+            card.ReturnToPosition(null);
+        }
+    }
+
+    private void DoRelease(Vector3 cursorPos)
+    {
+        CardSlot closestSlot = GetHoveredSlot(cursorPos);
+        CardSlot currentSlot = slots.FirstOrDefault(item => item.Occupant == SelectedCard);
+        if(currentSlot != null)
+        {
+            currentSlot.Occupant = null;
+        }
+        if (closestSlot != null)
+        {
+            closestSlot.Occupant = SelectedCard;
+        }
+    }
+
+    private void UpdateDrag(Vector3 cursorPos)
+    {
+        if(SelectedCard == null)
+        {
+            return;
+        }
+        Vector3 drag = cursorPos - cursorStartDragPoint;
+        SelectedCard.CardObject.transform.position = objStartDragPoint + drag;
+    }
+
+    private IReadOnlyList<CardInteractor> CreateCards()
+    {
+        List<CardInteractor> ret = new List<CardInteractor>();
         for (int value = 0; value < 52; value++)
         {
             Card card = Deck.Cards[value];
-            CardSelectorOption option = CreateOption(card);
+            CardInteractor option = CreateCard(card);
             ret.Add(option);
         }
         return ret;
     }
 
-    private CardSelectorOption CreateOption(Card card)
+    private CardInteractor CreateCard(Card card)
     {
-        GameObject optionObj = Instantiate(Mainscript.Instance.CardOptionPrefab);
-        optionObj.name = card.ToString();
-        optionObj.transform.parent = optionsMaster;
-        Material mat = optionObj.GetComponent<MeshRenderer>().material;
+        GameObject cardObj = Instantiate(Mainscript.Instance.CardOptionPrefab);
+        cardObj.name = card.ToString();
+        cardObj.transform.parent = CardOptionsPool;
+        Material mat = cardObj.GetComponent<MeshRenderer>().material;
         mat.SetFloat("_CardIndex", card.Value - 1);
         mat.SetFloat("_SuitIndex", card.Suit);
-        float x = card.Value * (1 + Mainscript.Instance.CardOptionMargin);
+        Transform basePosition = new GameObject("Option for " + card.ToString()).transform;
+        basePosition.parent = CardOptionsPool;
+        basePosition.localPosition = GetBasePosition(card);
+        cardObj.transform.localPosition = basePosition.localPosition;
+        return new CardInteractor(card, basePosition, cardObj);
+    }
+
+    private Vector3 GetBasePosition(Card card)
+    {
+        float x = (card.Value - 2) * (1 + Mainscript.Instance.CardOptionMargin);
+        float maxX = 13f * (1 + Mainscript.Instance.CardOptionMargin);
+        x -= maxX / 2;
+        x += .5f;
+        
         float y = card.Suit * (Mainscript.CardHeight + Mainscript.Instance.CardOptionMargin);
-        Transform target = new GameObject("Option for " + card.ToString()).transform;
-        target.parent = optionsMaster;
-        target.localPosition = new Vector3(x, y, 0);
-        return new CardSelectorOption(card, target, optionObj);
-    }
+        float maxY = 4f * (Mainscript.CardHeight + Mainscript.Instance.CardOptionMargin);
+        y -= maxY / 2;
+        y += Mainscript.CardHeight / 2;
 
-    private void Update()
-    {
-        bool isSelecting = Input.GetMouseButton(0);
-
-        PlaceMain();
-        UpdateOptionPositions(isSelecting);
-        UpdateSelectedness(isSelecting);
-    }
-
-    private void UpdateSelectedness(bool isSelecting)
-    {
-        CardSelectorOption hoveredCard = GetHoveredCard();
-        foreach (CardSelectorOption option in options)
-        {
-            if(isSelecting)
-            {
-                option.UpdateSelectedness(option == hoveredCard);
-            }
-            else
-            {
-                option.UpdateSelectedness(option == CurrentCard);
-            }
-        }
-        if(!isSelecting && wasSelecting)
-        {
-            CurrentCard = hoveredCard;
-        }
-        wasSelecting = isSelecting;
+        return new Vector3(x, y, 0);
     }
 
     Vector3 GetMousePos()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Plane plane = new Plane(Vector3.forward, 0);
+        Plane plane = new Plane(transform.forward, transform.position);
         float enter = 0;
         plane.Raycast(ray, out enter);
         return ray.GetPoint(enter);
     }
+
+    private CardSlot GetHoveredSlot(Vector3 cursorPos)
+    {
+        float minDist = (SelectedCard.BasePosition.position - cursorPos).sqrMagnitude;
+        CardSlot ret = null;
+        foreach (CardSlot slot in slots)
+        {
+            float dist = (slot.SlotPosition.position - cursorPos).sqrMagnitude;
+            if (dist < minDist)
+            {
+                minDist = dist;
+                ret = slot;
+            }
+        }
+        return ret;
+    }
     
-    private CardSelectorOption GetHoveredCard()
+    private CardInteractor GetHoveredCard(Vector3 cursorPos)
     {
         float minDist = float.MaxValue;
-        CardSelectorOption ret = CurrentCard;
-        Vector3 mousePos = GetMousePos();
-        foreach (CardSelectorOption item in options)
+        CardInteractor ret = SelectedCard;
+        foreach (CardInteractor item in cards)
         {
-            float dist = (item.Target.position - mousePos).sqrMagnitude;
+            float dist = (item.CardObject.transform.position - cursorPos).sqrMagnitude;
             if(dist < minDist)
             {
                 minDist = dist;
@@ -108,50 +194,41 @@ public class CardSelector : MonoBehaviour
         return ret;
     }
 
-    private void UpdateOptionPositions(bool isSelecting)
+    private class CardSlot
     {
-        float progressionTarget = isSelecting ? 0f : 1f;
-        Progression = Mathf.Lerp(Progression, progressionTarget, Time.deltaTime * 5);
-        foreach (CardSelectorOption option in options)
+        public Transform SlotPosition { get; }
+        public CardInteractor Occupant { get; set; }
+
+        public CardSlot(Transform slotPosition)
         {
-            option.UpdateOptionPosition(CurrentCard.Target.localPosition, Progression);
+            SlotPosition = slotPosition;
         }
     }
 
-    private void PlaceMain()
+    private class CardInteractor
     {
-        optionsMaster.localPosition = Vector3.Lerp(optionsMaster.localPosition, -CurrentCard.Target.localPosition, Time.deltaTime * 5);
-    }
-}
+        public Card Basis { get; }
+        public Transform BasePosition { get; }
+        public GameObject CardObject { get; }
 
+        private float selectedness;
 
-public class CardSelectorOption
-{
-    public Card Basis { get; }
-    public Transform Target { get; }
-    public GameObject OptionObject { get; }
+        public CardInteractor(Card basis, Transform basePosition, GameObject cardObject)
+        {
+            Basis = basis;
+            BasePosition = basePosition;
+            CardObject = cardObject;
+        }
 
-    private float selectedness;
+        public void ReturnToPosition(CardSlot slotPosition)
+        {
+            Vector3 targetPosition = slotPosition == null ? BasePosition.position : slotPosition.SlotPosition.position;
+            CardObject.transform.position = Vector3.Lerp(CardObject.transform.position, targetPosition, Time.deltaTime * 10);
+        }
 
-    public CardSelectorOption(Card basis, Transform target, GameObject optionObject)
-    {
-        Basis = basis;
-        Target = target;
-        OptionObject = optionObject;
-    }
-
-    public void UpdateOptionPosition(Vector3 selectedPosition, float progression)
-    {
-        Vector3 pos = Vector3.Lerp(Target.localPosition, selectedPosition, progression);
-        OptionObject.transform.localPosition = pos;
-    }
-
-    public void UpdateSelectedness(bool isSelected)
-    {
-        float selectednessTarget = isSelected ? 1 : 0;
-        selectedness = Mathf.Lerp(selectedness, selectednessTarget, Time.deltaTime * 5);
-        float xScale = 1f + selectedness / 10;
-        float yScale = Mainscript.CardHeight + selectedness * Mainscript.CardHeight / 10;
-        OptionObject.transform.localScale = new Vector3(xScale, yScale, 1);
+        public override string ToString()
+        {
+            return Basis.ToString();
+        }
     }
 }
